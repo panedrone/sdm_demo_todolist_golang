516c329123dde4b7a19d0f6601e19c24fea16d1f
panedrone <sqldalmaker@gmail.com>
2023-03-25 04:17:47 +0200

    refactor

--- raw_sql/router/project_handlers.go
+++ raw_sql/router/project_handlers.go
@@ -29,7 +29,7 @@ func ProjectCreate(ctx *gin.Context) {
 }
 
 func ProjectsReadAll(ctx *gin.Context) {
-	groups, err := gDao.GetProjects(ctx)
+	groups, err := gDao.GetProjectList(ctx)
 	if err != nil {
 		abortWith500(ctx, err.Error())
 		return
